7e951f9abe27b39201509fc0f51483baa8c9aa14
Chromilo Amin <chromiloamin@gmail.com>
2023-05-10 00:29:35 -0700

    fix: exercise2 operations.js

--- operations.js
+++ operations.js
@@ -1,38 +1,38 @@
-const asset = require('assert');
+const assert = require('assert');
 
-exports.insertDocument(db,document,collection,callback)=>{
+exports.insertDocument = (db,document,collection,callback)=>{
     const coll = db.collection(collection);
-    coll.inser(document,(err,result)=>{
+    coll.insert(document,(err,result)=>{
         assert.equal(err,null);
         console.log("Inserted " + result.result.n +
             " documents into the collection " + collection);
         callback(result);
     });
-});
+};
 
-exports.findDocument(db,collection,callback)=>{
+exports.findDocuments = (db,collection,callback)=>{
     const coll = db.collection(collection);
     coll.find({}).toArray((err,docs)=>{
         assert.equal(err,null);
         callback(docs);
     });
-});
+};
 
-exports.removeDocument(db,document,collection,callback)=>{
+exports.removeDocument = (db,document,collection,callback)=>{
     const coll = db.collection(collection);
     coll.deleteOne(document, (err,result)=>{
         assert.equal(err,null);
         console.log("Removed the document ", document);
         callback(result);
     });
-});
+};
 
-exports.updateDocument(db,document,update,collection,callback)=>{
+exports.updateDocument = (db,document,update,collection,callback)=>{
     const coll = db.collection(collection);
     coll.updateOne(document,{ $set:update},null,(err,result)=>{
         assert.equal(err,null);
         console.log("Updated the document ", update);
         callback(result);
     });
-});
+};
 
